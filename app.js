15a1600f05e8b0bda38963aa746d54339e038389
zbay <zlwilliams1@gmail.com>
2016-01-20 21:14:19 +0000

    Authentication

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index.js');
 var newShortcut = require('./routes/newShortcut.js');
 var savedShortcut = require('./routes/savedShortcut.js');
-mongoose.connect('mongodb://heroku_bdpzl0fg:shortening123@ds047085.mongolab.com:47085/heroku_bdpzl0fg', function (err, db)
+mongoose.connect('mongodb://heroku_bdpzl0fg:lvaq24pju65pel7t952fdpuvgk@ds047085.mongolab.com:47085/heroku_bdpzl0fg', function (err, db)
 //mongoose.connect('mongodb://localhost:27017/url-shortener-micro', function (err, db)
 {
  if (err) {
